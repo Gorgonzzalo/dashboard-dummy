bfc77d68ef324ce5c3f4b3fbe911ab265ba93156
Gonzalo dC <116873281+Gorgonzzalo@users.noreply.github.com>
2023-07-07 13:40:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,65 +46,74 @@ st.set_page_config(page_title='Power Plant Dashboard')
 
 # Sidebar
 st.sidebar.title('Dashboard Menu')
-selected_month = st.sidebar.selectbox('Select a month', ['June', 'July'])
+selected_power_plant = st.sidebar.selectbox('Select a power plant', ['Solar Power Plant', 'Wind Farm'])
 
 # Solar Power Plant page
-if selected_month == 'June':
-    st.title('Solar Power Plant Data - June')
-    st.write('Here is the data for the solar power plant in June:')
-    st.write(solar_df_june)
+if selected_power_plant == 'Solar Power Plant':
+    st.title('Solar Power Plant Data')
 
-    # Plot power output over time
-    plt.figure(figsize=(8, 6))
-    plt.plot(solar_df_june['Date'], solar_df_june['Power Output (kW)'], marker='o')
-    plt.xlabel('Date')
-    plt.ylabel('Power Output (kW)')
-    plt.title('Solar Power Plant - Power Output (June)')
-    plt.xticks(rotation=45)
-    st.pyplot(plt)
+    # Dropdown menu to select the month
+    selected_month_solar = st.selectbox('Select a month', ['June', 'July'])
 
-elif selected_month == 'July':
-    st.title('Solar Power Plant Data - July')
-    st.write('Here is the data for the solar power plant in July:')
-    st.write(solar_df_july)
+    if selected_month_solar == 'June':
+        st.write('Here is the data for the solar power plant in June:')
+        st.write(solar_df_june)
 
-    # Plot power output over time
-    plt.figure(figsize=(8, 6))
-    plt.plot(solar_df_july['Date'], solar_df_july['Power Output (kW)'], marker='o')
-    plt.xlabel('Date')
-    plt.ylabel('Power Output (kW)')
-    plt.title('Solar Power Plant - Power Output (July)')
-    plt.xticks(rotation=45)
-    st.pyplot(plt)
+        # Plot power output over time
+        plt.figure(figsize=(8, 6))
+        plt.plot(solar_df_june['Date'], solar_df_june['Power Output (kW)'], marker='o')
+        plt.xlabel('Date')
+        plt.ylabel('Power Output (kW)')
+        plt.title('Solar Power Plant - Power Output (June)')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
+
+    elif selected_month_solar == 'July':
+        st.write('Here is the data for the solar power plant in July:')
+        st.write(solar_df_july)
+
+        # Plot power output over time
+        plt.figure(figsize=(8, 6))
+        plt.plot(solar_df_july['Date'], solar_df_july['Power Output (kW)'], marker='o')
+        plt.xlabel('Date')
+        plt.ylabel('Power Output (kW)')
+        plt.title('Solar Power Plant - Power Output (July)')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
 
 # Wind Farm page
-if selected_month == 'June':
-    st.title('Wind Farm Data - June')
-    st.write('Here is the data for the wind farm in June:')
-    st.write(wind_df_june)
+elif selected_power_plant == 'Wind Farm':
+    st.title('Wind Farm Data')
 
-    # Plot power output over time
-    plt.figure(figsize=(8, 6))
-    plt.plot(wind_df_june['Date'], wind_df_june['Power Output (kW)'], marker='o')
-    plt.xlabel('Date')
-    plt.ylabel('Power Output (kW)')
-    plt.title('Wind Farm - Power Output (June)')
-    plt.xticks(rotation=45)
-    st.pyplot(plt)
+    # Dropdown menu to select the month
+    selected_month_wind = st.selectbox('Select a month', ['June', 'July'])
 
-elif selected_month == 'July':
-    st.title('Wind Farm Data - July')
-    st.write('Here is the data for the wind farm in July:')
-    st.write(wind_df_july)
+    if selected_month_wind == 'June':
+        st.write('Here is the data for the wind farm```python
+        st.write('Here is the data for the wind farm in June:')
+        st.write(wind_df_june)
 
-    # Plot power output over time
-    plt.figure(figsize=(8, 6))
-    plt.plot(wind_df_july['Date'], wind_df_july['Power Output (kW)'], marker='o')
-    plt.xlabel('Date')
-    plt.ylabel('Power Output (kW)')
-    plt.title('Wind Farm - Power Output (July)')
-    plt.xticks(rotation=45)
-    st.pyplot(plt)
+        # Plot power output over time
+        plt.figure(figsize=(8, 6))
+        plt.plot(wind_df_june['Date'], wind_df_june['Power Output (kW)'], marker='o')
+        plt.xlabel('Date')
+        plt.ylabel('Power Output (kW)')
+        plt.title('Wind Farm - Power Output (June)')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
+
+    elif selected_month_wind == 'July':
+        st.write('Here is the data for the wind farm in July:')
+        st.write(wind_df_july)
+
+        # Plot power output over time
+        plt.figure(figsize=(8, 6))
+        plt.plot(wind_df_july['Date'], wind_df_july['Power Output (kW)'], marker='o')
+        plt.xlabel('Date')
+        plt.ylabel('Power Output (kW)')
+        plt.title('Wind Farm - Power Output (July)')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
 
 # About page
 st.sidebar.title('About')
